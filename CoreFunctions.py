c6158b3f9bef10cabf4f6e76c88b02805d5d003c
mafiastewart <jamesdalmau@gmail.com>
2015-02-08 11:22:26 +1100

    11:22am Have got it reading from text file, not populating player attributes properly yet

--- CoreFunctions.py
+++ CoreFunctions.py
@@ -46,36 +46,40 @@ def PickRandomItemFromList(List1):
 
 def CreatePlayerList():
     global PlayerID
-    PlayerID += 1
-    Alignment = ""
-    Team = ""
-    Survivor = ""
-    BelovedPrincess = ""
-    LynchBomb = ""
-    NightBomb = ""
-    InnocentChild = ""
-    FriendlyNeighbour = ""
-    Saulus = ""
-    Judas = ""
-    Cop = ""
-    Doctor = ""
-    Roleblocker = ""
-    Busdriver = ""
-    Vigilante = ""
-    TeamRecruiter = ""
-    DeputyCop = ""
-    DeputyDoctor = ""
-    DeputyRoleblocker = ""
-    DeputyBusdriver = ""
-    DeputyVigilante = ""
-    DeputyTeamRecruiter = ""
-    NightKillResistant = ""
-    LynchResistant = ""
-    AddPlayer(PlayerID, Alignment, Team, Survivor, BelovedPrincess, LynchBomb, NightBomb, InnocentChild,
-              FriendlyNeighbour, Saulus, Judas, Cop, Doctor, Roleblocker, Busdriver, Vigilante, TeamRecruiter,
-              DeputyCop, DeputyDoctor, DeputyRoleblocker, DeputyBusdriver, DeputyVigilante, DeputyTeamRecruiter,
-              NightKillResistant, LynchResistant)
-
+    global PlayerList
+    PlayerID=1
+    PlayerSetupFile = list(open('players.txt', 'r')) #Read players.txt and create a list
+    Alignment=""            # "Town" or "Mafia" or "Neither"
+    Team=""                      # 0 (for no team) or the number of the team (Town and Mafia team numbers must be different)
+    Survivor=""               # "Yes" or "No" (If yes, Alignment should be "Neither" and Team should be 0)
+    BelovedPrincess=""        # "Yes" or "No" (If yes, Alignment should be "Town")
+    LynchBomb=""              # "Yes" or "No"
+    NightBomb=""              # "Yes" or "No"
+    InnocentChild=""          # "Yes" or "No"
+    FriendlyNeighbour=""      # "Yes", "Odd", "Even" or "No"
+    Saulus=""                 # "Yes" or "No" (If yes, Alignment should be "Mafia" and Team should be 0)
+    Judas=""                  # "Yes" or "No" (If yes, Alignment should be "Town")
+    Cop=""                    # "Yes", "Odd", "Even" or "No" --- note, must add record of who the cop has investigated
+    Doctor=""                 # "Yes", "Odd", "Even" or "No"
+    Roleblocker=""            # "Yes", "Odd", "Even" or "No"
+    Busdriver=""              # "Yes", "Odd", "Even" or "No"
+    Vigilante=""              # "Yes", "Odd", "Even" or "No"
+    TeamRecruiter=""          # "Yes" or "No" (If yes, Alignment should be "Town")
+    DeputyCop=""              # "Yes" or "No"
+    DeputyDoctor=""           # "Yes" or "No"
+    DeputyRoleblocker=""      # "Yes" or "No"
+    DeputyBusdriver=""        # "Yes" or "No"
+    DeputyVigilante=""        # "Yes" or "No"
+    DeputyTeamRecruiter=""    # "Yes" or "No"
+    NightKillResistant=""        # -1 (for invulnerable), 0 (for normal), otherwise X-shot
+    LynchResistant=""            # -1 (for invulnerable), 0 (for normal), otherwise X-shot
+    for LineFromTextFile in PlayerSetupFile:
+        if LineFromTextFile[:3] == '***':
+            AddPlayer(PlayerID, Alignment, Team, Survivor, BelovedPrincess, LynchBomb, NightBomb, InnocentChild, FriendlyNeighbour, Saulus, Judas, Cop, Doctor, Roleblocker, Busdriver, Vigilante, TeamRecruiter, DeputyCop, DeputyDoctor, DeputyRoleblocker, DeputyBusdriver, DeputyVigilante, DeputyTeamRecruiter,  NightKillResistant, LynchResistant)
+            PlayerID += 1
+        else:
+            exec(LineFromTextFile)
+    print(PlayerList)
 
 # noinspection PyUnusedLocal
 def AddPlayer(PlayerIDToInsert, Alignment, Team, Survivor, BelovedPrincess, LynchBomb, NightBomb, InnocentChild,
@@ -83,12 +87,21 @@ def AddPlayer(PlayerIDToInsert, Alignment, Team, Survivor, BelovedPrincess, Lync
               DeputyCop, DeputyDoctor, DeputyRoleblocker, DeputyBusdriver, DeputyVigilante, DeputyTeamRecruiter,
               NightKillResistant, LynchResistant):
     global PlayerList
-    PlayerToAdd = []
+    PlayerToAdd = {}
     PlayerToAdd['Playerid'] = PlayerIDToInsert
+    AttributeToAdd = ""
     for Attribute in PlayerAttributes:
-        exec("PlayerToAdd['" + Attribute + "']=" + Attribute)
-    PlayerToAdd['HasInvestigated'] = 0
-    PlayerToAdd['BeingBusDrivenWith'] = 0
-    PlayerToAdd['NumberOfNamesInHat'] = 100
+        exec("AttributeToAdd = '" + Attribute + "'")
+        exec("PlayerToAdd['" + Attribute + "'] = '" + AttributeToAdd + "'")
+    if InnocentChild == "Yes":
+        PlayerToAdd['NumberOfNamesInHat'] = 0
+    else:
+        PlayerToAdd['NumberOfNamesInHat'] = 100
     PlayerList.append(PlayerToAdd)
+    print(PlayerToAdd)
+    print(PlayerList)
     PlayerToAdd.clear()
+
+
+InitiateVariables()
+CreatePlayerList()
