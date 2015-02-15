3096908fe85ae9b47763bebc4b9f3016a0575f33
mafiastewart <jamesdalmau@gmail.com>
2015-02-15 18:15:44 +1100

    Have done generalisable "TryToPickTownPlayer" and "TryToPickMafiaPlayer" for night actions

--- CoreFunctions.py
+++ CoreFunctions.py
@@ -51,7 +51,10 @@ def ReturnOneListWithCommonItemsFromFourLists(List1, List2, List3, List4):  # Pu
 
 
 def PickRandomItemFromList(List1):  # Pure list function
-    return List1[randint(0, len(List1) - 1)]
+    if List1 != []:
+        return List1[randint(0, len(List1) - 1)]
+    else:
+        return()
 
 
 def CreatePlayerList(): # Reads players.txt and makes the main list for use in a single game
@@ -131,7 +134,7 @@ def TryToLynch():
     PlayerWhoWillBeLynched = 0
     while len(Candidates) > 0 and PlayerWhoWillBeLynched == 0:  # While there are still candidates and no one has been voted for
         #print ("The current candidates for the lynch are " + str(Candidates))
-        CandidatePickedFromHat = PickNameFromHat(Candidates)
+        CandidatePickedFromHat = PickNameFromHatForLynch(Candidates)
         #print ("Going to see if there are enough votes for " + str(CandidatePickedFromHat))
         GetsEnoughVotes, ActualVoters = WillGetEnoughLynchVotes(CandidatePickedFromHat)
         if GetsEnoughVotes == "Yes":    # See if this candidate gets enough votes
@@ -176,12 +179,16 @@ def PunishAndRewardVotersAfterLynch(Voters,LynchedPlayer):
     for Voter in Voters:
         if AlignmentOfDeadPlayer == 'Mafia':
             ChangeToNumberOfNamesInHat = math.ceil(Day * 1.3 * randint(3,5))
-            WriteAttributeToPlayer(Voter,'NumberOfNamesInHat',GetAttributeFromPlayer(Voter,'NumberOfNamesInHat')-ChangeToNumberOfNamesInHat)
-            print("Player " + str(Voter) + " voted to lynch a scum. Their odds are now " + str(GetAttributeFromPlayer(Voter,'NumberOfNamesInHat')))
+            NewNumber=GetAttributeFromPlayer(Voter,'NumberOfNamesInHat')-ChangeToNumberOfNamesInHat
+            print("Player " + str(Voter) + " voted to lynch a mafia.")
         elif AlignmentOfDeadPlayer == 'Town':
             ChangeToNumberOfNamesInHat = math.ceil(Day * 1.3 * randint(3,5))
-            WriteAttributeToPlayer(Voter,'NumberOfNamesInHat',GetAttributeFromPlayer(Voter,'NumberOfNamesInHat')+ChangeToNumberOfNamesInHat)
-            print("Player " + str(Voter) + " voted to lynch a town. Their odds are now " + str(GetAttributeFromPlayer(Voter,'NumberOfNamesInHat')))
+            NewNumber=GetAttributeFromPlayer(Voter,'NumberOfNamesInHat')+ChangeToNumberOfNamesInHat
+            print("Player " + str(Voter) + " voted to lynch a town.")
+        if NewNumber<0:
+            NewNumber=0
+        WriteAttributeToPlayer(Voter,'NumberOfNamesInHat',NewNumber)
+        print("Player " + str(Voter) + "'s odds are now " + str(NewNumber))
     #Punish and reward the non-voters
     NonVoters = SearchPlayersFor('Alive','==','"Yes"')
     for NonVoter in NonVoters:
@@ -189,16 +196,19 @@ def PunishAndRewardVotersAfterLynch(Voters,LynchedPlayer):
             if NonVoter not in Voters:
                 if AlignmentOfDeadPlayer == 'Mafia':
                     ChangeToNumberOfNamesInHat = math.ceil(Day * 1.3 * randint(2,4))
-                    WriteAttributeToPlayer(NonVoter,'NumberOfNamesInHat',GetAttributeFromPlayer(NonVoter,'NumberOfNamesInHat')+ChangeToNumberOfNamesInHat)
-                    print("Player " + str(NonVoter) + " didn't vote to lynch a scum. Their odds are now " + str(GetAttributeFromPlayer(NonVoter,'NumberOfNamesInHat')))
+                    NewNumber = GetAttributeFromPlayer(NonVoter,'NumberOfNamesInHat')+ChangeToNumberOfNamesInHat
+                    print("Player " + str(NonVoter) + " didn't vote to lynch a mafia.")
                 elif AlignmentOfDeadPlayer == 'Town':
                     ChangeToNumberOfNamesInHat = math.ceil(Day * 1.3 * randint(2,4))
-                    WriteAttributeToPlayer(NonVoter,'NumberOfNamesInHat',GetAttributeFromPlayer(NonVoter,'NumberOfNamesInHat')-ChangeToNumberOfNamesInHat)
-                    print("Player " + str(NonVoter) + " didn't vote to lynch a town. Their odds are now " + str(GetAttributeFromPlayer(NonVoter,'NumberOfNamesInHat')))
+                    NewNumber = GetAttributeFromPlayer(NonVoter,'NumberOfNamesInHat')-ChangeToNumberOfNamesInHat
+                    print("Player " + str(NonVoter) + " didn't vote to lynch a town.")
+                if NewNumber<0:
+                    NewNumber=0
+                WriteAttributeToPlayer(NonVoter,'NumberOfNamesInHat',NewNumber)
+                print("Player " + str(NonVoter) + "'s odds are now " + str(GetAttributeFromPlayer(NonVoter,'NumberOfNamesInHat')))
 
 
-
-def PickNameFromHat(PlayersToGoInHat):
+def PickNameFromHatForLynch(PlayersToGoInHat):
     Hat = []
     for Player in PlayersToGoInHat:
         i = 0
@@ -206,6 +216,102 @@ def PickNameFromHat(PlayersToGoInHat):
             Hat.append(Player)
             i +=1
     return(PickRandomItemFromList(Hat))
+
+
+def TryToPickTownPlayer(PlayerWhoIsChoosing,PlayersNotEligible):
+    UnfilteredListOfPlayersForHat = []
+    PlayersToGoInHat = []
+    Hat = []
+    PlayersTeam = GetAttributeFromPlayer(PlayerWhoIsChoosing,"Team")
+    PlayersAlignment = GetAttributeFromPlayer(PlayerWhoIsChoosing,"Alignment")
+    if PlayersTeam == 0:
+        #If the player is not on a team, the pool of possible townies is everyone who is still alive
+        UnfilteredListOfPlayersForHat = ReturnOneListWithCommonItemsFromTwoLists(SearchPlayersFor("Alive","==","'Yes'"),SearchPlayersFor("PlayerID","!=",PlayerWhoIsChoosing))
+    else:
+        #If the player is on a team, the pool depends on the player's alignment
+        if PlayersAlignment == "Mafia": # If player is mafia, list excludes that mafia team
+            UnfilteredListOfPlayersForHat = ReturnOneListWithCommonItemsFromTwoLists(SearchPlayersFor("Alive","==","'Yes'"),SearchPlayersFor("Team","!=",PlayersTeam))
+        else: #If player isn't Mafia, list includes everyone
+            UnfilteredListOfPlayersForHat = ReturnOneListWithCommonItemsFromTwoLists(SearchPlayersFor("Alive","==","'Yes'"),SearchPlayersFor("PlayerID","!=",PlayerWhoIsChoosing))
+    if PlayersNotEligible != []: #Exclude the players who are to be excluded
+        for UnfilteredPlayer in UnfilteredListOfPlayersForHat:
+            if UnfilteredPlayer not in PlayersNotEligible:
+                PlayersToGoInHat.append(UnfilteredPlayer)
+    else:
+        PlayersToGoInHat = UnfilteredListOfPlayersForHat
+    for PlayerInHat in PlayersToGoInHat: #Now go through each player who's going into the hat
+        NumberOfNamesFromPlayerList = int(GetAttributeFromPlayer(PlayerInHat,'NumberOfNamesInHat'))
+        if NumberOfNamesFromPlayerList >=130 and GetAttributeFromPlayer(PlayerInHat,'InnocentChild') != "Yes": #If the candidate is probably mafia, don't add them.
+            NumberOfTimesToGoInHat = 0
+        else: #If the candidate might not be mafia, figure out how many names to add (reversing polarity)
+            if NumberOfNamesFromPlayerList >= 100:
+                NumberOfTimesToGoInHat = 100 - (NumberOfNamesFromPlayerList - 100)
+            else:
+                NumberOfTimesToGoInHat = 100 + (100 - NumberOfNamesFromPlayerList)
+            if GetAttributeFromPlayer(PlayerInHat,'InnocentChild') == "Yes":
+                NumberOfTimesToGoInHat = NumberOfTimesToGoInHat * 3
+            if PlayersTeam != 0 and PlayersAlignment == "Town":
+                if PlayersTeam == int(GetAttributeFromPlayer(PlayerInHat,"'Team'")):
+                    NumberOfTimesToGoInHat = NumberOfTimesToGoInHat * 3
+        #Now populate the hat
+        i = 0
+        while i < NumberOfTimesToGoInHat:
+            Hat.append(PlayerInHat)
+            i +=1
+    if Hat != []:
+        return(PickRandomItemFromList(Hat))
+    else:
+        return(0)
+
+
+def TryToPickMafiaPlayer(PlayerWhoIsChoosing,PlayersNotEligible):
+    UnfilteredListOfPlayersForHat = []
+    PlayersToGoInHat = []
+    Hat = []
+    PlayersTeam = GetAttributeFromPlayer(PlayerWhoIsChoosing,"Team")
+    PlayersAlignment = GetAttributeFromPlayer(PlayerWhoIsChoosing,"Alignment")
+    if PlayersTeam == 0:
+        #If the player is not on a team, the pool of possible mafia is everyone who is still alive
+        UnfilteredListOfPlayersForHat = ReturnOneListWithCommonItemsFromTwoLists(SearchPlayersFor("Alive","==","'Yes'"),SearchPlayersFor("PlayerID","!=",PlayerWhoIsChoosing))
+    else:
+        #If the player is on a team, the pool depends on the player's alignment
+        if PlayersAlignment == "Mafia": # If player is mafia, list includes everyone
+            UnfilteredListOfPlayersForHat = ReturnOneListWithCommonItemsFromTwoLists(SearchPlayersFor("Alive","==","'Yes'"),SearchPlayersFor("PlayerID","!=",PlayerWhoIsChoosing))
+        else: #If player isn't Mafia, list excludes members of the player's team
+            UnfilteredListOfPlayersForHat = ReturnOneListWithCommonItemsFromTwoLists(SearchPlayersFor("Alive","==","'Yes'"),SearchPlayersFor("Team","!=",PlayersTeam))
+    if PlayersNotEligible != []: #Exclude the players who are to be excluded
+        for UnfilteredPlayer in UnfilteredListOfPlayersForHat:
+            if UnfilteredPlayer not in PlayersNotEligible:
+                PlayersToGoInHat.append(UnfilteredPlayer)
+    else:
+        PlayersToGoInHat = UnfilteredListOfPlayersForHat
+    for PlayerInHat in PlayersToGoInHat: #Now go through each player who's going into the hat
+        NumberOfNamesFromPlayerList = int(GetAttributeFromPlayer(PlayerInHat,'NumberOfNamesInHat'))
+        if GetAttributeFromPlayer(PlayerInHat,'InnocentChild') != "Yes": #Exclude any innocent children
+            NumberOfTimesToGoInHat = 0
+        else:
+            if PlayersTeam != 0: # if the player is on a team
+                if PlayersTeam == int(GetAttributeFromPlayer(PlayerInHat,"'Team'")): #if the candidate is on the same team
+                    if PlayersAlignment == "Town":
+                        NumberOfNamesFromPlayerList = 0
+                    elif PlayersAlignment == "Mafia":
+                        NumberOfTimesToGoInHat = NumberOfNamesFromPlayerList * 3
+                else:   #If chooser's team is not candidate's team
+                    NumberOfTimesToGoInHat = NumberOfNamesFromPlayerList
+            else:   # if chooser is not on a team
+                NumberOfTimesToGoInHat = NumberOfNamesFromPlayerList
+            if NumberOfTimesToGoInHat <= 70: #Exclude anyone who's unlikely to be mafia
+                NumberOfTimesToGoInHat = 0
+        #Now populate the hat
+        i = 0
+        while i < NumberOfTimesToGoInHat:
+            Hat.append(PlayerInHat)
+            i +=1
+    if Hat != []:
+        return(PickRandomItemFromList(Hat))
+    else:
+        return(0)
+
 
 
 def WillGetEnoughLynchVotes(TargetPlayerID):
@@ -253,22 +359,29 @@ def ShuffleList(InputList):
     return ReturnList
 
 
-def BuildListOfMafiaTeamNumbers():   #Return a list of the numbers of the scum teams that have living players
+def BuildListOfTeamNumbers(Alignment):   #Return a list of the numbers of the teams that have living players
+    #Alignment = "Mafia", "Town", ""
     LivingPlayers = SearchPlayersFor('Alive',"==","'Yes'")
-    MafiaTeams = []
-    for Player in LivingPlayers: #Build a list of the scum teams
-        if GetAttributeFromPlayer(Player,'Alignment') == 'Mafia':
-            TeamNumberForThisPlayer = GetAttributeFromPlayer(Player,'Team')
+    Teams = []
+    for Player in LivingPlayers: #Build a list of the teams
+        IsTeamAppropriateToAddToList = "No"
+        TeamNumberForThisPlayer = GetAttributeFromPlayer(Player,'Team')
+        if Alignment == "":
             if TeamNumberForThisPlayer != 0:
-                if TeamNumberForThisPlayer not in MafiaTeams:
-                    MafiaTeams.append(GetAttributeFromPlayer(Player,'Team'))
-    return(MafiaTeams)
+                IsTeamAppropriateToAddToList = "Yes"
+        elif GetAttributeFromPlayer(Player,'Alignment') == Alignment:
+            if TeamNumberForThisPlayer != 0:
+                IsTeamAppropriateToAddToList = "Yes"
+        if IsTeamAppropriateToAddToList == "Yes":
+            if TeamNumberForThisPlayer not in Teams:
+                Teams.append(GetAttributeFromPlayer(Player,'Team'))
+    return(Teams)
 
 
 def SeeIfAnyOneMafiaTeamHasTheMajority():
     LivingPlayers = SearchPlayersFor('Alive',"==","'Yes'")
     Majority = NumberOfVotesRequiredToLynch()
-    MafiaTeams = BuildListOfMafiaTeamNumbers()
+    MafiaTeams = BuildListOfTeamNumbers("Mafia")
     print("Seeing if any one MafiaTeam out of Teams " + str(MafiaTeams) + " have " + str(Majority) + " votes.")
     for TeamNumber in MafiaTeams: #See if each scum team has the votes
         ListOfLivingPlayersInTeam = ReturnOneListWithCommonItemsFromTwoLists(SearchPlayersFor('Alive',"==","'Yes'"), SearchPlayersFor('Team',"==",TeamNumber))
@@ -315,6 +428,7 @@ def SimulateSingleGame():
             #Night cycle
             print()
             print("Night " + str(Night))
+            NightRoutine()
             CheckForVictory()
             if WinningTeam != '':
                 DayOrNightWhenGameEnded = "N" + str(Night)
@@ -326,6 +440,61 @@ def SimulateSingleGame():
     LivingPlayers = SearchPlayersFor('Alive','==',"'Yes'")
     print()
     print("Winners = " + WinningTeam)
+
+
+def NightRoutine():
+    DoAllRoleblocking()
+    #DoAllBusdriving()
+    #DoAllInvestigating()
+    #DoAllProtecting()
+    #DoAllTeamNightKills()
+    #DoAllVigilanteKills()
+
+
+def DoAllRoleblocking():
+    global PlayersBeingRoleblocked
+    global Night
+    PlayersBeingRoleblocked = []
+    LivingRoleblockers = ReturnOneListWithCommonItemsFromTwoLists(SearchPlayersFor('Alive','==',"'Yes'"),SearchPlayersFor("Roleblocker","!=","'No'"))
+    if LivingRoleblockers != []: #If there are any roleblockers
+        for Player in LivingRoleblockers:
+            RoleblockerActiveTonight = "No"
+            #See if this roleblocker is to be active on this particular night
+            RoleblockerValueFromPlayerlist = GetAttributeFromPlayer(Player,"Roleblocker")
+            if RoleblockerValueFromPlayerlist == "Yes":
+                RoleblockerActiveTonight = "Yes"
+            elif RoleblockerValueFromPlayerlist == IsNumberOddOrEven(Night):
+                RoleblockerActiveTonight = "Yes"
+            if RoleblockerActiveTonight == "Yes":
+                if GetAttributeFromPlayer(Player,'Alignment') == 'Mafia':
+                    PlayerToBeRoleblocked = TryToPickTownPlayer(Player,[])
+                else:
+                    PlayerToBeRoleblocked = TryToPickMafiaPlayer(Player,[])
+                if PlayersBeingRoleblocked != 0:
+                    PlayersBeingRoleblocked.append(PlayerToBeRoleblocked)
+                    print("On this night, Player " + str(Player) + " is roleblocking " + str(PlayerToBeRoleblocked))
+        print("The following players are being roleblocked = " + str(PlayersBeingRoleblocked))
+
+
+def DoAllBusdriving():
+    global Busdrivings
+    Busdrivings = []
+    LivingBusdrivers = ReturnOneListWithCommonItemsFromTwoLists(SearchPlayersFor('Alive','==',"'Yes'"),SearchPlayersFor("Busdriver","==","'Yes'"))
+    if LivingBusdrivers != []: #If there are any Busdrivers
+        for Player in LivingBusdrivers:
+            BusdriverActiveTonight = "No"
+            #See if this Busdriver is to be active on this particular night
+            BusdriverValueFromPlayerlist = GetAttributeFromPlayer(Player,"Busdriver")
+            if BusdriverValueFromPlayerlist == "Yes":
+                BusdriverActiveTonight = "Yes"
+            elif BusdriverValueFromPlayerlist == IsNumberOddOrEven(Night):
+                BusdriverActiveTonight = "Yes"
+            if Player in PlayersBeingRoleblocked:
+                BusdriverActiveTonight = "No"
+            if BusdriverActiveTonight == "Yes":
+                PlayerToBeRoleblocked = PickNameFromHatForRoleblock(Player)
+                PlayersBeingRoleblocked.append(PlayerToBeRoleblocked)
+                print("On this night, Player " + str(Player) + " is roleblocking " + str(PlayerToBeRoleblocked))
 
 InitiateGlobalVariables()
 SimulateSingleGame()
